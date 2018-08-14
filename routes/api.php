0414ed02458c95940f5a64aa190c75a0d277f090
kratos619 <gaurav.palaspagar@hotmail.com>
2018-08-14 15:09:16 +0530

    Init Project Aftre Creating API and installing npm

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// list articles
+Route::get('articles','ArticallControler@index');
+
+// list Singlel articles
+Route::get('article/{id}','ArticallControler@show');
+
+// create new  articles
+Route::post('article','ArticallControler@store');
+
+// update  articles
+Route::put('article','ArticallControler@store');
+
+//  delete  articles
+Route::delete('article/{id}','ArticallControler@destroy');
+
+
